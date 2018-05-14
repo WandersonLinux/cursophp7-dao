8d77aae251cd66107f0796a375fc7ba2c81f053b
Wanderson Pereira da Silva <wandersonmetal123@hotmail.com>
2018-05-14 15:57:17 -0300

    criamos exemplos de busca de usuario

--- index.php
+++ index.php
@@ -10,9 +10,27 @@ echo json_encode($usuarios);
 
 */
 
-$root = new Usuario();
+//carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
 
-$root->loadbyId(3);
+//echo $root;
 
-echo $root;
+//carrega lista de usuaários
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+//carrega uma lista de usuarios buscando pelo login
+/*$search = Usuario::search("jo");
+
+echo json_encode($search);
+
+*/
+//carregar um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("wanderson","1234567890");
+
+echo $usuario;
  ?>
